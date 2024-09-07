64e00a9bb3878566dc1caf12cf72067f9f945328
douglasmyu <douglasmyu@csu.fullerton.edu>
2024-09-06 23:47:46 -0700

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<head>
+    <title>this is a header</title>
+</head>
+<body>
+    <h1>this is a body</h1>
+
+</body>
+<footer>
+    <p>this is a footer</p>
+</footer>
+
+
+<style>
+    body{
+        background-color:brown ;
+    }
+    p{
+        font-size: 2em;
+        font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    }
+</style>
